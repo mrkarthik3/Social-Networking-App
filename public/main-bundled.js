e4937b881c162b13655f59e35fb006854bc469bf
Sai Karthik <jskarthik1@gmail.com>
2021-05-14 07:31:38 +0530

    Frontend JS, Webpack, Babel introduced for Live Search Feature

---
+++ public/main-bundled.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";new function n(){!function(n,s){if(!(n instanceof s))throw new TypeError("Cannot call a class as a function")}(this,n),alert("search js is successfully working")}})();
